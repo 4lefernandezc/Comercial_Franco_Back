abd5def235cdbc139639b89c1ad3439f7809e77a
sebastianDLL <delgadillosebastian75@gmail.com>
2025-03-18 18:21:24 -0400

    Agregar validación para asegurar que la nueva contraseña no sea igual a la contraseña actual en el DTO de cambio de contraseña.

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, MinLength, MaxLength } from 'class-validator';
+import { IsString, MinLength, MaxLength, Validate } from 'class-validator';
+import { PasswordsNotEqualConstraint } from '../decorators/password.decorator';
 
 export class ChangePasswordDto {
   @ApiProperty()
@@ -12,5 +13,6 @@ export class ChangePasswordDto {
   @IsString()
   @MinLength(6, { message: 'La nueva contraseña debe tener al menos 6 caracteres.' })
   @MaxLength(20, { message: 'La nueva contraseña no debe exceder los 20 caracteres.' })
+  @Validate(PasswordsNotEqualConstraint)
   newPassword: string;
 }
