66569f4616ddd4a8ca5d7814f8b16951cdb01626
sebastianDLL <delgadillosebastian75@gmail.com>
2025-03-18 18:21:29 -0400

    Aumentar el límite de intentos de inicio de sesión y la ventana de tiempo en el middleware de limitación de tasa

--- src/middlewares/login-rate-limit.middleware.ts
+++ src/middlewares/login-rate-limit.middleware.ts
@@ -4,8 +4,8 @@ import rateLimit from 'express-rate-limit';
 @Injectable()
 export class LoginRateLimitMiddleware implements NestMiddleware {
   private limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutos
-    max: 5, // Máximo 5 intentos en la ventana de tiempo
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 50, // Máximo 50 intentos en la ventana de tiempo
     message: {
       statusCode: 429,
       error: 'Too Many Requests',
